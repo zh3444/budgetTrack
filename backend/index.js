40399751fc49afa62b947e4298c0e6d025ccb0f2
zh3444 <zhihaoteo34@gmail.com>
2025-06-18 14:02:20 +0800

    Fixed .env

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,5 @@
 /* eslint-disable no-undef */
-require('dotenv').config();
+require('dotenv').config({ path: '../.env' });
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
