b28832b63de016e23808b0c286e409aaa54317c6
zh3444 <zhihaoteo34@gmail.com>
2025-06-19 12:10:37 +0800

    Added user and transaction schemasz

--- backend/models/User.js
+++ backend/models/User.js
@@ -2,7 +2,12 @@ import mongoose from 'mongoose';
 
 const userSchema = new mongoose.Schema({
   name: String,
-  email: String
+  email: String,
+  password: String,
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
 });
 
 export const User = mongoose.model('User', userSchema);
