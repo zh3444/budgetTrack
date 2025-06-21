0c8effd51c2006004c58c8f4a9ef7443d2cd1eb2
zh3444 <zhihaoteo34@gmail.com>
2025-06-21 20:44:53 +0800

    Configured path imports

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+/* eslint-disable no-undef */
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
+import svgr from 'vite-plugin-svgr';
+import path from 'path';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss(), svgr()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, './src'),
+    }
+  },
+  server: {
+    host: true,
+    open: true
+  }
 })
