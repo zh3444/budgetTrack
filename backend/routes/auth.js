b09e699a92023b5d1fabeb839f248c87c72cebf7
zh3444 <zhihaoteo34@gmail.com>
2025-06-21 20:52:33 +0800

    Backend: setup of authentication workflow

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,6 +1,7 @@
+/* eslint-disable no-undef */
 import express from 'express';
 import jwt from 'jsonwebtoken';
-import bcrypt from 'bcrypt';
+import bcrypt from 'bcryptjs';
 import dotenv from 'dotenv';
 import { User } from '../models/User.js';
 
@@ -8,36 +9,69 @@ dotenv.config({ path: '../../.env' });
 const router = express.Router();
 const JWT_SECRET = process.env.JWT_SECRET;
 
+/**
+ * Middleware to authenticate a token from the request header.
+ * 
+ * This function checks for an authorization token in the request header.
+ * If no token is found, it returns a 401 status with a 'Missing token' message.
+ * If a token is found, it verifies the token using the JWT secret.
+ * Upon successful verification, it attaches the decoded token to the request object.
+ * If the token verification fails, it returns a 403 status with an 'Invalid or expired token' message.
+ * 
+ * @param {Object} req - Express request object.
+ * @param {Object} res - Express response object.
+ * @param {Function} next - Express next middleware function.
+ * 
+ * @returns {void}
+ */
+
+export const authenticateToken = (req, res, next) => {
+  const authHeader = req.headers['authorization'];
+  const token = authHeader?.split(' ')[1];
+
+  if (!token) {
+    return res.status(401).json({ message: 'Missing token' });
+  }
+  try {
+    const decoded = jwt.verify(token, JWT_SECRET);
+    req.user = decoded;
+    next();
+  } catch (err) {
+    return res.status(403).json({ message: 'Invalid or expired token ', err});
+  }
+}
+
 // for authenticating a user
 router.post('/login', async (req, res) => {
-  const { username, password } = req.body;
-  const user = await User.findOne({ name: username });
+  const { email, password } = req.body;
+  const user = await User.findOne({ email });
 
   if (!user || !(await bcrypt.compare(password, user.password))) {
-    return res.status(401).json({ message: 'Invalid username or password' });
+    return res.status(401).json({ message: 'Invalid email or password' });
   }
 
-  const token = jwt.sign({ userId: user._id }, JWT_SECRET, { expiresIn: '100h'});
+  const token = jwt.sign({ userId: user._id, username: user.name }, JWT_SECRET, { expiresIn: '100h'});
   res.json({ token });
 });
 
 // for registering a new user
 router.post('/register', async (req, res) => {
   const { name, email, password, date } = req.body;
-  console.log('print request body ', req.body);
+  // hashing the input password
+  const hashedPassword = await bcrypt.hash(password, 10);
   try {
-    const result = await User.create({
+    await User.create({
       name,
       email,
-      password,
+      password: hashedPassword,
       createdAt: date
     });
     res.status(200).json({
       message: 'User created successfully',
-      data: result
+      status: 200
     });
   } catch (err) {
-    res.status(500).json({error: err.message});
+    res.status(500).json({ error: err.message });
   }
 });
 
