45d1fed31cf1805aacb1155d48fd7bdc200ecc59
zh3444 <zhihaoteo34@gmail.com>
2025-07-09 11:19:05 +0800

    Feat: homepage to check for gmail token

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,40 +1,36 @@
 import { useEffect } from "react";
+import { checkGmailTokenStatus } from "./api";
 
 import { Button } from "@/components/ui/button";
 
 const Dashboard = () => {
-  // test gmail auth flow after login
-  // useEffect(() => {
-  //   window.location.href = `${import.meta.env.VITE_API_URL}/gmail/google`;
-  // }, []);
-
-  const test = () => {
-    window.location.href = `${import.meta.env.VITE_API_URL}/gmail/google`;
-  }
-
+  // Check if the user has a valid Gmail token on component mount
   useEffect(() => {
-    getEmails();
+    async function checkTokenStatus() {
+      const status = await checkGmailTokenStatus(localStorage.getItem('token'));
+      if (status.needsReauth) {
+        const originalEmail = localStorage.getItem('email');
+        const array = new Uint32Array(1);
+        window.crypto.getRandomValues(array);
+        const nonce = array[0].toString(16);
+        const state = encodeURIComponent(JSON.stringify({
+          email: originalEmail,
+          nonce: nonce
+        }));
+        console.log('Token needs re-authentication ', status);
+        window.location.href = `${import.meta.env.VITE_API_URL}/gmail/google?state=${state}`;
+      } else {
+        console.log('Token is valid, no re-authentication needed ', status);
+      }
+    }
+    checkTokenStatus();
   }, []);
-
-  const getEmails = async () => {
-    const response = await fetch(`${import.meta.env.VITE_API_URL}/gmail/emails`, {
-      credentials: 'include'
-    });
-    const data = await response.json();
-    console.log('getting emails ',data);
-  }
 
   return (
     <>
       <h2 className="text-2xl font-bold mb-4">
         Dashboard
       </h2>
-      <Button onClick={test}>
-        Test
-      </Button>
-      <Button onClick={getEmails}>
-        Get Emails
-      </Button>
       <p> Your recent transactions will appear here </p>
     </>
   );
