09633ed8322cdc9c58674355aa849a9c8bdfde64
zh3444 <zhihaoteo34@gmail.com>
2025-07-09 10:50:10 +0800

    Updated schemas

---
+++ email-worker/models/Transaction.js
@@ -0,0 +1,32 @@
+import mongoose from 'mongoose';
+
+const transactionSchema = new mongoose.Schema({
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  },
+  amount: {
+    type: Number,
+    required: true
+  },
+  merchant: {
+    type: String,
+    required: true
+  },
+  category: {
+    type: String,
+    required: true
+  },
+  date: {
+    type: Date,
+    default: Date.now
+  },
+  type: {
+    type: String,
+    enum: ['income', 'expense'],
+    default: 'expense',
+  }
+});
+
+export const Transaction = mongoose.model('Transaction', transactionSchema);
